d5aa7aaa6816700fbfe5e4dc846f54fa8978d790
sunjq1996 <sunjq1996@163.com>
2022-06-07 10:49:19 +0800

    【特性】集成ufo界面

--- source/src/components/ufo.vue
+++ source/src/components/ufo.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    <div class="cursor">
+  <div style='width:100vw;height: 100vh;'>
+    <night-sky id='nightSky' layers='3' density='146' velocity='122' starcolor='#ffffff' />
+
+    <div id='content'>
+      <div class='beam'>
+        <img id="island" src="../assets/images/island-withname.png">
+      </div>
+      <div class='cursor'></div>
     </div>
   </div>
 </template>
 
 <script>
-import { cursorMove } from 'ufo-cursor'
+import {
+  cursorMove
+} from 'ufo-cursor'
 import 'ufo-cursor/ufocursor.css'
+import '@h0rn0chse/night-sky/dist/bundle.min.js'
 
 export default {
   name: 'ufo',
@@ -19,4 +28,27 @@ export default {
 </script>
 
 <style>
+  #content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+  }
+
+  #nightSky {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  #island {
+    width: 30vw;
+    height: auto;
+  }
 </style>
